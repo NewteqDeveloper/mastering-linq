4959baf5165a64b532d709f71174432fec66b95f
Newteq <newt.fourie@entelect.co.za>
2019-10-08 17:26:56 +0200

    Initial Commit

---
+++ MasteringLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MasteringLinq.SelectQuery;
+
+namespace MasteringLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var masteringSelect = new MasteringSelect();
+            masteringSelect.Run();
+
+            Console.WriteLine("All code has finished running");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MasteringLinq/SelectQuery/MasteringSelect.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasteringLinq.SelectQuery
+{
+    public class MasteringSelect
+    {
+        public void Run()
+        {
+            var list = new List<SelectModel>
+            {
+                new SelectModel
+                {
+                    Name = "Newt",
+                    Gender = Gender.Male,
+                    Age = 27,
+                },
+                new SelectModel
+                {
+                    Name = "Alicia",
+                    Gender = Gender.Female,
+                    Age = 27,
+                }
+            };
+
+            StandardSelect(list);
+
+            var result = list.Select(SelectWithMethod);
+        }
+
+        private IEnumerable<string> StandardSelect(IEnumerable<SelectModel> list)
+        {
+            return list.Select(x => x.Name);
+        }
+
+        private string SelectWithMethod(SelectModel model)
+        {
+            return model.Name;
+        }
+    }
+
+    public class SelectModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
